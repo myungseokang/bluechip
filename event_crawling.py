7a806ceed2e4fac3378e03eb7b835217bb4fd87a
EDCMTC <qazxsw3256@naver.com>
2016-10-21 13:16:25 +0900

    all_event() 삭제 business() 생성 주석 참고

--- event_crawling.py
+++ event_crawling.py
@@ -3,32 +3,49 @@ import requests
 import lxml
 import time
 
-def all_event():
-    page = 0
-    url = "http://finance.daum.net/quote/marketvalue.daum"
+def business():
+    """
+    반환 : [{'business_name':업종 이름, 'event':종목 이름, 'code':종목코드, 'price':종목 현재가, 'change':등락률 }, ....]
+    기타 업종 제외.
+    """
+    base_url="http://finance.daum.net/quote/upjong_sub.daum?stype=P"
+    html_doc = requests.get(base_url)
+    html = BeautifulSoup(html_doc.text, 'lxml')
+    business_html = html.find('table', {'id':'bizBody1'}).find_all('tr')
     event_set = []
-    while(1):
-        page += 1
-        html_doc = requests.get(url, params={'page':page})
-        html = BeautifulSoup(html_doc.text, 'lxml')
-        events = html.find_all('td', {'class':'txt'})
-        for event in events:
-            """
-            event 모델을 장고에서 만들면 해당 데이터로 레코드를 만든다
-            """
-            event_set.append({})
-            event_set[len(event_set)-1]['event'] = event.a.text
-            event_set[len(event_set)-1]['code'] = event.a.get('href').split('=')[1]
+    for business in business_html:
         try:
-            if page%10 == 0:
-                    html.find('span', {'class':'jumpNext'}).a.text
-            elif page%10 == 1:
-                    html.find('span', {'class':'on'}).b.text
-            else:
-                html.find('a', {"href":"javascript:getPage(\'"+str(page+1)+"\');"}).text
+            name = business.find('td', {'class':'txt'}).text
+            number = business.find('td', {'class':'txt'}).a.get('href')[-2:]
         except:
-            break
-    return event_set
+            continue
+        page = 0
+        while(1):
+            page += 1
+            url = "http://finance.daum.net/quote/upjong_detail_sub.daum?stype=P&col=pchgrate&order=desc"
+            html_doc = requests.get(url, params={'seccode':number,'page':page})
+            html = BeautifulSoup(html_doc.text, 'lxml')
+            events_html = html.find_all('tr')
+            for event_html in events_html:
+                try:
+                    event = event_html.find('td', {'class':'txt'}).text
+                    code = event_html.find('td', {'class':'txt'}).a.get('href')[-6:]
+                    price = event_html.find_all('td', {'class':'cUp'})[0].text
+                    change = event_html.find_all('td', {'class':'cUp'})[1].text
+                except:
+                    continue
+                else:
+                    event_set.append({'business_name':name, 'event':event, 'code':code, 'price':price, 'change':change})
+            try:
+                if page%10 == 0:
+                        html.find('span', {'class':'jumpNext'}).a.text
+                elif page%10 == 1:
+                        html.find('a', {"href":"javascript:goPage(\'"+str(page+1)+"\');"}).text
+                else:
+                    html.find('a', {"href":"javascript:goPage(\'"+str(page+1)+"\');"}).text
+            except:
+                break
+        return event_set
 
 def graph_url(code):
     """
