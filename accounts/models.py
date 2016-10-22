10e8a065d262f3623b9a8b5018d9af72f77e28f8
Leop0ld <l3opold7@gmail.com>
2016-10-22 14:32:56 +0900

    기본 세팅 및 수정

--- accounts/models.py
+++ accounts/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
 class InvestUser(AbstractUser):
     """
     모의 투자 유저
--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import InvestUser
+
+
+class InvestUserTestCase(TestCase):
+    def setUp(self):
+        testuser = InvestUser.objects.create(username='testuser')
+
+    def test_user_can_create(self):
+        """
+        Create user test
+        """
+        testuser = InvestUser.objects.get_or_404(username='testuser')
+        self.assertEqual(testuser.username, 'testuser')
