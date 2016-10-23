f131df72e1a2bd1c0763f24abfc0604d426268e0
Leop0ld <l3opold7@gmail.com>
2016-10-23 16:11:09 +0900

    Coverage 및 Travis ci 적용

--- stock/models.py
+++ stock/models.py
@@ -7,8 +7,32 @@ class Stock(models.Model):
     business = models.CharField(max_length=30)
     title = models.CharField(max_length=20, unique=True, null=False)
     code = models.CharField(max_length=10, unique=True, null=False)
-    price = models.PositiveSmallIntegerField()
-    user = models.ManyToManyField(InvestUser, through='StockUser', blank=True)
+
+    # 전일
+    yesterday_price = models.PositiveIntegerField(default=0)
+
+    # 현재가
+    price = models.PositiveIntegerField(default=0)
+
+    # 시가
+    today_start_price = models.PositiveIntegerField(default=0)
+
+    # 상한가
+    max_price = models.PositiveIntegerField(default=0)
+
+    # 하한가
+    min_price = models.PositiveIntegerField(default=0)
+
+    # 고가
+    high_price = models.PositiveIntegerField(default=0)
+
+    # 저가
+    low_price = models.PositiveIntegerField(default=0)
+
+    # 등락률
+    change = models.FloatField(default=0)
+
+    users = models.ManyToManyField(StockUser, blank=True)
 
     def __str__(self):
         return self.title
