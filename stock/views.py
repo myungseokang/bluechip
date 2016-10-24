d06dbd76f03dbe609842fe7b89f3be3576d996c7
EDCMTC <qazxsw3256@naver.com>
2016-10-24 22:37:16 +0900

    소스 수정

--- stock/views.py
+++ stock/views.py
@@ -4,9 +4,8 @@ from django.views.generic.list import ListView
 from .models import Stock
 
 def main(request):
-    first = Stock.objects.all().order_by('-price')[0]
-    graph_url = first.graph_url()
-    return render(request, 'stock/main.html', {"first":first,"graph":graph_url})
+    first = Stock.objects.all().order_by('-price')[0] # 현재가가 가장 높은 주식
+    return render(request, 'stock/main.html', {"first":first})
 
 class StockLV(ListView):
     template_name = 'stock/stock_list.html'
