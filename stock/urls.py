d09cc50b6564c9d908ce83c87100554bbd4fff80
EDCMTC <qazxsw3256@naver.com>
2016-10-25 00:52:17 +0900

    search url 생성

--- stock/urls.py
+++ stock/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
-from .views import StockLV, main, stockDV
+from .views import StockLV, main, stockDV, stock_search
 
 
 urlpatterns = [
     url(r'^main/$', main, name='main'),
-    url(r'^detail/(?P<code>[0-9]+)/$', stockDV, name='stock_detail'),
     url(r'^$', StockLV.as_view(), name='list'),
+    url(r'^detail/(?P<code>[A-Za-z-0-9]+)/$', stockDV, name='stock_detail'),
+    url(r'^search/$', stock_search, name='search'),
 ]
