ffe1ad80c60b20d6169ec4bd146223c615c62a46
EDCMTC <qazxsw3256@naver.com>
2016-10-25 02:57:36 +0900

    어드민에 StockManger 추가

--- stock/admin.py
+++ stock/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from stock.models import Stock
+from stock.models import Stock, StockManager
 # Register your models here.
 
 admin.site.register(Stock)
+admin.site.register(StockManager)
