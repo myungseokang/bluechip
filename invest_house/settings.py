89a1f8bdc2aac5d2c1c3d6f121b50cd0d1d71f0e
EDCMTC <qazxsw3256@naver.com>
2016-10-27 02:18:10 +0900

    주식 분석 앱 생성, setting 추가

--- invest_house/settings.py
+++ invest_house/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'accounts.apps.AccountsConfig',
     'stock.apps.StockConfig',
     'django_extensions',
+    'stock_analsis.app.StockAnalysisConfig',
 ]
 
 MIDDLEWARE = [
---
+++ stock_analysis/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StockAnalysisConfig(AppConfig):
+    name = 'stock_analysis'
