255576c1aba8ca303bb9a3ef7bf613f21a177e1c
Leop0ld <l3opold7@gmail.com>
2016-10-26 09:38:00 +0900

    init_stock 수정

--- scripts/init_stock.py
+++ scripts/init_stock.py
@@ -2,22 +2,20 @@ from event_crawling import business
 from stock.models import Stock
 
 
-def create_stock():
+def create_stock_data():
     stock_list = business()
     for stock in stock_list:
-        Stock.objects.create(business=stock['business'], code=stock['code'], title=stock['title'])
-
-
-def create_stock_data():
-    stock_list = Stock.objects.all()
-    for stock in stock_list:
-        stock.graph_url()
-        stock.stock_reset()
+        new_stock = Stock()
+        new_stock.business = stock['business']
+        new_stock.code = stock['code']
+        new_stock.title = stock['title']
+        new_stock.graph_url()
+        new_stock.stock_reset()
+        new_stock.save()
 
 
 def run():
     try:
-        create_stock()
         create_stock_data()
     except Exception as e:
         print(e)
