59d297b7c4bb920e2a1547d1077d99874431eecf
EDCMTC <qazxsw3256@naver.com>
2016-10-29 02:00:38 +0900

    거래량 반환

--- stock_analysis/views.py
+++ stock_analysis/views.py
@@ -1,12 +1,17 @@
 from django.shortcuts import render
 
+from stock.models import Stock
 # Create your views here.
 
 def trade(request):
-    return render(request, 'stock_analsis/trade.html')
+    trade_list = Stock.objects.all().order_by('-deal')
+    context = {
+        'trade_list':trade_list,
+    }
+    return render(request, 'stock_analysis/trade.html', context)
 
 def increase(request):
-    return render(request, 'stock_analsis/increase.htm')
+    return render(request, 'stock_analysis/increase.htm')
 
 def decrease(request):
-    return render(request, 'stock_analsis/decrease.html')
+    return render(request, 'stock_analysis/decrease.html')
