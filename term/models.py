1e1af0de55d9058047cfc53a481407b9581c2863
EDCMTC <qazxsw3256@naver.com>
2016-11-01 15:14:55 +0900

    term models 생성

--- term/models.py
+++ term/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Term(models.Model):
+    term = models.CharField(max_length=100, unique=True)
+    mean = models.CharField(max_length=1000)
--- term/views.py
+++ term/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 
+from .models import Term
 # Create your views here.
 
 def term(request):
-    return render(request, 'term/term.html')
+    terms = Term.objects.all()
+    context = {
+        'terms':terms
+    }
+    return render(request, 'term/term.html', terms)
