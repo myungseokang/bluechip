61ea25b583957c5deb22012fcd2cafafb3c145f4
EDCMTC <qazxsw3256@naver.com>
2016-11-02 23:22:57 +0900

    튜토리얼 디자인 적용

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import url
 
-from .views import tutorial_1
+from .views import tutorial_1,tutorial_2,tutorial_3,tutorial_4,tutorial_5,tutorial_6,tutorial_7,tutorial_8,tutorial_9,tutorial_10
 
 urlpatterns = [
-    url(r'^one/$', tutorial_1, name='tutorial_1')
+    url(r'^1/$', tutorial_1, name='1'),
+    url(r'^2/$', tutorial_2, name='2'),
+    url(r'^3/$', tutorial_3, name='3'),
+    url(r'^4/$', tutorial_4, name='4'),
+    url(r'^5/$', tutorial_5, name='5'),
+    url(r'^6/$', tutorial_6, name='6'),
+    url(r'^7/$', tutorial_7, name='7'),
+    url(r'^8/$', tutorial_8, name='8'),
+    url(r'^9/$', tutorial_9, name='9'),
+    url(r'^10/$', tutorial_10, name='10'),
 ]
--- tutorial/views.py
+++ tutorial/views.py
@@ -3,3 +3,30 @@ from django.shortcuts import render
 # Create your views here.
 def tutorial_1(request):
     return render(request, 'tutorial/tutorial_1.html')
+
+def tutorial_2(request):
+    return render(request, 'tutorial/tutorial_2.html')
+
+def tutorial_3(request):
+    return render(request, 'tutorial/tutorial_3.html')
+
+def tutorial_4(request):
+    return render(request, 'tutorial/tutorial_4.html')
+
+def tutorial_5(request):
+    return render(request, 'tutorial/tutorial_5.html')
+
+def tutorial_6(request):
+    return render(request, 'tutorial/tutorial_6.html')
+
+def tutorial_7(request):
+    return render(request, 'tutorial/tutorial_7.html')
+
+def tutorial_8(request):
+    return render(request, 'tutorial/tutorial_8.html')
+
+def tutorial_9(request):
+    return render(request, 'tutorial/tutorial_9.html')
+
+def tutorial_10(request):
+    return render(request, 'tutorial/tutorial_10.html')
