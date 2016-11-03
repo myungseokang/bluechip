4db711518aa776aae61c9466795c7d942e54c857
EDCMTC <qazxsw3256@naver.com>
2016-11-03 10:29:06 +0900

    튜토리얼 js, 백엔드 적용

--- tutorial/views.py
+++ tutorial/views.py
@@ -7,26 +7,62 @@ def tutorial_1(request):
 def tutorial_2(request):
     return render(request, 'tutorial/tutorial_2.html')
 
-def tutorial_3(request):
-    return render(request, 'tutorial/tutorial_3.html')
+def tutorial_3(request, stock):
+    context = {
+        'stock':stock,
+    }
+    return render(request, 'tutorial/tutorial_3.html', context)
 
-def tutorial_4(request):
-    return render(request, 'tutorial/tutorial_4.html')
+def tutorial_4(request, stock, price1):
+    context = {
+        'stock':stock,
+        'price1':price1,
+    }
+    return render(request, 'tutorial/tutorial_4.html', context)
 
-def tutorial_5(request):
-    return render(request, 'tutorial/tutorial_5.html')
+def tutorial_5(request, stock, price1):
+    context = {
+        'stock':stock,
+        'price1':price1,
+    }
+    return render(request, 'tutorial/tutorial_5.html', context)
 
-def tutorial_6(request):
-    return render(request, 'tutorial/tutorial_6.html')
+def tutorial_6(request, stock, price1):
+    context = {
+        'stock':stock,
+        'price1':price1,
+    }
+    return render(request, 'tutorial/tutorial_6.html', context)
 
-def tutorial_7(request):
-    return render(request, 'tutorial/tutorial_7.html')
+def tutorial_7(request, stock, price1):
+    context = {
+        'stock':stock,
+        'price1':price1,
+    }
+    return render(request, 'tutorial/tutorial_7.html', context)
 
-def tutorial_8(request):
-    return render(request, 'tutorial/tutorial_8.html')
+def tutorial_8(request, stock, price1, price2):
+    context = {
+        'stock':stock,
+        'price1':price1,
+        'price2':price2,
+    }
+    return render(request, 'tutorial/tutorial_8.html', context)
 
-def tutorial_9(request):
-    return render(request, 'tutorial/tutorial_9.html')
+def tutorial_9(request, stock, price1, price2):
+    result = int(price2)-int(price1)
+    context = {
+        'price1':price1,
+        'stock':stock,
+        'price2':price2,
+        'result':result
+    }
+    return render(request, 'tutorial/tutorial_9.html', context)
 
-def tutorial_10(request):
-    return render(request, 'tutorial/tutorial_10.html')
+def tutorial_10(request, stock, price1, price2):
+    context = {
+        'price1':price1,
+        'stock':stock,
+        'price2':price2,
+    }
+    return render(request, 'tutorial/tutorial_10.html', context)
