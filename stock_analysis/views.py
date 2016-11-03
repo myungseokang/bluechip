a3e7ae29ad8074f3e12ba695ed18d2f817f86560
EDCMTC <qazxsw3256@naver.com>
2016-11-03 21:13:55 +0900

    거래량, 상승, 하락 + 순위 번호 야매로 해결

--- stock_analysis/views.py
+++ stock_analysis/views.py
@@ -8,53 +8,81 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 def trade(request):
     trade_list = Stock.objects.all().order_by('-deal')
-    paginator = Paginator(trade_list, 30)
+    paginator = Paginator(trade_list, 100)
 
     page = request.GET.get('page', '')
+
+    try:
+        if(int(page)==1):
+            add = 0
+        else:
+            add = int(page) * 100
+    except:
+        pass
+
     try:
         trade_page = paginator.page(page)
     except PageNotAnInteger:
+        add = 0
         trade_page = paginator.page(1)
     except EmptyPage:
+        add =  paginator.end_index() * 100
         trade_page = paginator.page(paginator.num_pages)
     context = {
         'trade_page': trade_page,
         'pagination_range': paginator.page_range,
         'searchForm': searchForm(),
+        'add':add
     }
     return render(request, 'stock_analysis/trade.html', context)
 
 
 def increase(request):
     increase_list = Stock.objects.filter(change__gt=0).order_by('-change')
-    paginator = Paginator(increase_list, 30)
+    paginator = Paginator(increase_list, 100)
     page = request.GET.get('page', '')
+
+    try:
+        if(int(page)==1):
+            add = 0
+        else:
+            add = int(page) * 100
+    except:
+        pass
+
     try:
         increase_page = paginator.page(page)
     except PageNotAnInteger:
+        add = 0
         increase_page = paginator.page(1)
     except EmptyPage:
+        add =  paginator.end_index() * 100
         increase_page = paginator.page(paginator.num_pages)
     context = {
-        'increase_page': increase_page,
+        'trade_page': increase_page,
         'pagination_range': paginator.page_range,
         'searchForm': searchForm(),
+        'add':add
     }
     return render(request, 'stock_analysis/increase.html', context)
 
 def decrease(request):
     decrease_list = Stock.objects.filter(change__lt=0).order_by('change')
-    paginator = Paginator(decrease_list, 30)
+    paginator = Paginator(decrease_list, 100)
     page = request.GET.get('page', '')
+
     try:
         decrease_page = paginator.page(page)
     except PageNotAnInteger:
+        add = 0
         decrease_page = paginator.page(1)
     except EmptyPage:
+        add =  paginator.end_index() * 100
         decrease_page = paginator.page(paginator.num_pages)
     context = {
-        'decrease_page': decrease_page,
+        'trade_page': decrease_page,
         'pagination_range': paginator.page_range,
         'searchForm': searchForm(),
+        'add':add
     }
     return render(request, 'stock_analysis/decrease.html', context)
